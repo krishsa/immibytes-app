26fdf2d4a0233ebc7cad1d81ae312a05cd4fd065
Krishnaveni RL <krishnaveni.rl@gmail.com>
2018-07-23 00:36:25 -0500

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import HomePage from './pages/HomePage';
+
+const element = <HomePage />;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class HomePage extends React.Component {
+    render(){
+        return(
+            <div> 
+                <div>
+                    Immibytes - Welcomes you.. Site Construction inprogress
+                </div>
+            </div>
+        );
+    }
+}
