e6c7be6b73bda79d771ab237cd285db74f19a6d4
Krishnaveni RL <krishnaveni.rl@gmail.com>
2018-08-01 21:27:08 -0500

    Added Menu for the page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import {Button} from '@material-ui/core';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 export default class HomePage extends React.Component {
-    render(){
+    state = {
+        anchorEl: null,
+    };
+
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
+    render() {
+        const { anchorEl } = this.state;
+
         return(
             <div> 
                 <div>
-                    Immibytes - Welcomes you.. Site Construction inprogress
+                    Immibytes - Welcomes you.... Site Construction inprogress
+                 
                 </div>
-                <Button variant="contained" color="primary">
-                    Hello World
+                
+                <Button 
+                variant="contained" color="primary"
+                aria-owns = {anchorEl? 'simple-menu' : null} 
+                aria-haspopup= "true" 
+                onClick = {this.handleClick} >
+
+                    OpenMenu
+
                 </Button>
+                <Menu 
+                    id="simple-menu"
+                    anchorEl = {anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={this.handleClose}
+                 >
+                    <MenuItem>Profile</MenuItem>
+                    <MenuItem>My Account</MenuItem>
+                    <MenuItem>Logout</MenuItem>
+                </Menu>
             </div>
         );
     }
